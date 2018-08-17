f2fdbd5ff500b5e97319004738d17a59dae59350
Roman Hryshchyshyn <roman.hryshchyshyn@jetsoftpro.com>
2018-08-17 11:42:58 +0300

    Initial Commit

---
+++ 303Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  303Test
+//
+//  Created by Roman on 8/17/18.
+//  Copyright © 2018 JetSoftPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
